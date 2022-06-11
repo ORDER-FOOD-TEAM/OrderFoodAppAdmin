a078f0071602a409c485e4eac5135c3d54899020
baoloc7401 <locltb@vitalify.asia>
2022-06-11 21:37:54 +0700

    Added Analyze activity

--- app/src/main/java/com/example/orderfoodappadmin/activity/LoadingActivity.kt
+++ app/src/main/java/com/example/orderfoodappadmin/activity/LoadingActivity.kt
@@ -1,12 +1,16 @@
 package com.example.orderfoodappadmin.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.orderfoodappadmin.R
 
-class FoodDetail : AppCompatActivity() {
+class LoadingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_food_detail)
+        setContentView(R.layout.activity_loading)
+    }
+
+    fun startLoadingDialog(){
+
     }
 }
